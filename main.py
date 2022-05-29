5104b8e802ec99c10747c5f78d7240c8e44c394f
HM0809 <47137792+hm0809@users.noreply.github.com>
2022-05-29 18:34:39 +0930

    Improved Aesthetics, Functionality+++, UI Better

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 global_settings = {"cpu_setting" : 0, "save_file" : True}
+global_items = {}
 def dropRates():
     print("This calculator will tell you how many runs on average it will take to find the items you are looking for.")
     print("Note: choosing the high performance setting may cause performance issues if your drop rates are particularly low.")
@@ -14,24 +15,23 @@ def dropRates():
     
 
     global_settings["cpu_setting"] = [1000, 10000, 50000][int(input("CPU-Usage: '1' - (low - only for very low probability tests), '2' - (medium), '3' - (high - freaky are ya?): "))-1]
-    #item_num = int(input("How many items are you trying to find (must be from the same source)? "))
-    item_num = 0
+    item_num = int(input("How many items are you trying to find (must be from the same source)? "))
     items = {"name" : [], "p" : [], "total_prob" : 0, "cumulative_prob" : 0}
-    while True:
-        item_num += 1
-        items["name"].append(input(f"What is the name of item {item_num}? "))
+    for i in range(item_num):
+        items["name"].append(input(f"What is the name of item {i+1}? "))
         try:
-            items["p"].append(float(input(f"What is the drop rate (0 < p <= 1) of {items['name'][item_num-1]}? ")))
+            items["p"].append(float(input(f"What is the drop rate (0 < p <= 1) of {items['name'][i]}? ")))
         except ValueError as e:
             print("That is not a valid integer between 0 and 1")
-            items["p"].append(float(input(f"What is the drop rate (0 < p <= 1) of {items['name'][item_num-1]}? ")))
-        if input("Any more items? Enter any key if yes, otherwise enter 'n': ").lower() == "n":
-            break
+            items["p"].append(float(input(f"What is the drop rate (0 < p <= 1) of {items['name'][i]}? ")))
+
     
     items["total_prob"] = sum(items["p"])
     items["cumulative_prob"] = reduce(mul, items["p"])
     items["iterations_required"] = round((1/items["cumulative_prob"]) * 10)
 
+    global global_items
+    global_items = items
 
 
     return (items)
@@ -66,13 +66,29 @@ def numerical_dist(items):
     return (average_repetitions, repetition_list)
         
 def describe_probability():
+
+    #ONLY FOR PLOTTING FOR NO OVERFILL - DOES NOT AFFECT SUMMARY STATISTICS
+    def reject_outliers(data, m = 5.16):
+        d = np.abs(data - np.median(data))
+        mdev = np.median(d)
+        s = d/mdev if mdev else 0.
+        return data[s<m]
+
     values = numerical_dist(dropRates())
+
+    global global_items
+    key_values = {"items" : global_items["name"], "p" : global_items["p"]}
+    figure_df = pd.DataFrame(key_values)
+    print(figure_df)
     df = pd.DataFrame(values[1])
     #df.to_csv("probabilityDistribution.csv")
+    plt.style.use('fivethirtyeight')
     plt.hist(df, bins=np.linspace(0, max(values[1]), num=10000), cumulative=True, density=True, histtype='bar')
     plt.axvline(df.mean()[0], color='k', linestyle='dashed', linewidth=1)
-    plt.figtext(0, 0.05, str(df.describe(percentiles=[0.01, 0.05, .25, .5, .75, 0.95, 0.99])), va="top", ha="left")
+    plt.figtext(0.1, -0.05, str(df.describe(percentiles=[0.01, 0.05, .25, .5, .75, 0.95, 0.99]))[20::], va="top", ha="left")
+    plt.figtext(0.9, -0.05, figure_df.to_string(index=False), va='top', ha='right')
     plt.xlabel("Amount of attemps required")
+    plt.xlim((len(global_items["name"]), reject_outliers(df).dropna().max()[0]))
     plt.ylabel("Probability of drop by this attempt")
     plt.savefig("distribution.svg", bbox_inches = "tight")
     plt.tight_layout()
@@ -80,6 +96,6 @@ def describe_probability():
     Unless you have a supercomputer don't uncomment the plt.show, saving as an svg or png is faster, easier and better! 
     """
     #plt.show()
-    print(str(df.describe(percentiles=[0.01, 0.05, .25, .5, .75, 0.95, 0.99])))
 if __name__ == '__main__':
     describe_probability()
+    pass
